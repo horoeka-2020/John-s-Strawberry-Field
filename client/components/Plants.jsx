618d93cf2b6b4cc455791cec9df73dc45178845b
Kittisan P <70613193+Kittisan-P@users.noreply.github.com>
2020-11-06 11:04:41 +1300

    Fix package JSON and add the index page

--- client/components/Plants.jsx
+++ client/components/Plants.jsx
@@ -1,22 +1,23 @@
-// import React from 'react'
+import React from 'react'
 // import plants from '../../data/plants'
 // import { Link } from 'react-router-dom'
 
-// const Plants = () => {
-//   const rankKeys = Object.keys(ranks)
-//   return (
-//     <div>
-//       <h2>Nav</h2>
-//       <ul>
-//         {rankKeys.map((rank) => {
-//           return <Link to={`/list/${rank}`}>
-//             <li key={rank}>{rank}</li>
-//           </Link>
-//         })}
+const Plants = () => {
+  // const rankKeys = Object.keys(ranks)
+  return (
+    <div>
+      <h2>Plants</h2>
+      <ul>
+        <li>Build your ranks list here</li>
+        {/* {rankKeys.map((rank) => {
+          return <Link to={`/list/${rank}`}>
+            <li key={rank}>{rank}</li>
+          </Link>
+        })} */}
 
-//       </ul>
-//     </div>
-//   )
-// }
+      </ul>
+    </div>
+  )
+}
 
-// export default Plants
+export default Plants
