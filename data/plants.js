ad22cc0502f8b66cb74564ad47a610242c21950b
Caz <cjpitcher@gmail.com>
2020-11-05 23:04:37 +1300

    Added image property to plants

--- data/plants.js
+++ data/plants.js
@@ -1,155 +1,179 @@
 const plants = {
-    Spring: [
-      {
-        id: 1,
-        name: '',
-        type: 'vegetable',
-        planting: ''
-      },
-      {
-        id: 2,
-        name: '',
-        type: 'vegetable',
-        planting: ''
-      },
-      {
-        id: 3,
-        name: '',
-        type: 'fruit',
-        planting: ''
-      },
-      {
-        id: 4,
-        name: '',
-        type: 'fruit',
-        planting: ''
-      },
-      {
-        id: 5,
-        name: '',
-        type: 'flower',
-        planting: ''
-      },
-      {
-        id: 6,
-        name: '',
-        type: 'flower',
-        planting: ''
-      },
-    ],
-    Summer: [
+  Spring: [
     {
-        id: 7,
-        name: '',
-        type: 'vegetable',
-        planting: ''
-        },
-        {
-        id: 8,
-        name: '',
-        type: 'vegetable',
-        planting: ''
-        },
-        {
-        id: 9,
-        name: '',
-        type: 'fruit',
-        planting: ''
-        },
-        {
-        id: 10,
-        name: '',
-        type: 'fruit',
-        planting: ''
-        },
-        {
-        id: 11,
-        name: '',
-        type: 'flower',
-        planting: ''
-        },
-        {
-        id: 12,
-        name: '',
-        type: 'flower',
-        planting: ''
-        },
-    ],
-    Autumn: [
+      id: 1,
+      name: '',
+      type: 'vegetable',
+      planting: '',
+      image: ''
+    },
     {
-        id: 13,
-        name: '',
-        type: 'vegetable',
-        planting: ''
-        },
-        {
-        id: 14,
-        name: '',
-        type: 'vegetable',
-        planting: ''
-        },
-        {
-        id: 15,
-        name: '',
-        type: 'fruit',
-        planting: ''
-        },
-        {
-        id: 16,
-        name: '',
-        type: 'fruit',
-        planting: ''
-        },
-        {
-        id: 17,
-        name: '',
-        type: 'flower',
-        planting: ''
-        },
-        {
-        id: 18,
-        name: '',
-        type: 'flower',
-        planting: ''
-        },
-    ],
-    Winter: [
+      id: 2,
+      name: '',
+      type: 'vegetable',
+      planting: '',
+      image: ''
+    },
     {
-        id: 19,
-        name: 'Kale',
-        type: 'vegetable',
-        planting: 'The best times to plant are early in the morning or late in the day, so the plants aren’t exposed to the hot sun straight away. Find a full sun, sheltered position to plant and space at least 30cm - 50cm apart, to ensure the crops can fully mature and are not fighting for space, fertiliser and water.'
-        },
-        {
-        id: 20,
-        name: 'Carrots',
-        type: 'vegetable',
-        planting: 'Sow seed for the winter carrot crop in mid-summer, and deep mulch the bed in November with shredded leaves or straw. This insulating layer should be topped with an old row cover or bed sheet to hold it in place.'
-        },
-        {
-        id: 21,
-        name: 'Strawberries',
-        type: 'fruit',
-        planting: 'There are three main types. Plant June-bearing for one big crop in late spring or early summer, everbearing for spring and fall crops, and day-neutral for a large crop in spring and smaller harvests all summer.'
-        },
-        {
-        id: 22,
-        name: 'Peaches',
-        type: 'fruit',
-        planting: 'Plant trees against a southfacing wall under an eave, and prune them into a fan shape. And if you’re short on space, plant three or four varieties in one hole, pruning off all but the outwardfacing branches.'
-        },
-        {
-        id: 23,
-        name: 'Pansies',
-        type: 'flower',
-        planting: 'Pansies like full or partial sun, but need cooler temperatures to thrive. The ideal planting site will get morning sun but avoid the heat of the late afternoon.'
-        },
-        {
-        id: 24,
-        name: 'Hellebores',
-        type: 'flower',
-        planting: 'Hellebores prefer to grow in rich well-drained soil in full to part shade and whilst they are shade lovers most do better with a little sun, under the canopy of a deciduous tree or shrub is an ideal location.'
-        },
+      id: 3,
+      name: '',
+      type: 'fruit',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 4,
+      name: '',
+      type: 'fruit',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 5,
+      name: '',
+      type: 'flower',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 6,
+      name: '',
+      type: 'flower',
+      planting: '',
+      image: ''
+    },
+  ],
+  Summer: [
+    {
+      id: 7,
+      name: '',
+      type: 'vegetable',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 8,
+      name: '',
+      type: 'vegetable',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 9,
+      name: '',
+      type: 'fruit',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 10,
+      name: '',
+      type: 'fruit',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 11,
+      name: '',
+      type: 'flower',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 12,
+      name: '',
+      type: 'flower',
+      planting: '',
+      image: ''
+    },
+  ],
+  Autumn: [
+    {
+      id: 13,
+      name: '',
+      type: 'vegetable',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 14,
+      name: '',
+      type: 'vegetable',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 15,
+      name: '',
+      type: 'fruit',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 16,
+      name: '',
+      type: 'fruit',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 17,
+      name: '',
+      type: 'flower',
+      planting: '',
+      image: ''
+    },
+    {
+      id: 18,
+      name: '',
+      type: 'flower',
+      planting: '',
+      image: ''
+    },
+  ],
+  Winter: [
+    {
+      id: 19,
+      name: 'Kale',
+      type: 'vegetable',
+      planting: 'The best times to plant are early in the morning or late in the day, so the plants aren’t exposed to the hot sun straight away. Find a full sun, sheltered position to plant and space at least 30cm - 50cm apart, to ensure the crops can fully mature and are not fighting for space, fertiliser and water.',
+      image: ''
+    },
+    {
+      id: 20,
+      name: 'Carrots',
+      type: 'vegetable',
+      planting: 'Sow seed for the winter carrot crop in mid-summer, and deep mulch the bed in November with shredded leaves or straw. This insulating layer should be topped with an old row cover or bed sheet to hold it in place.',
+      image: ''
+    },
+    {
+      id: 21,
+      name: 'Strawberries',
+      type: 'fruit',
+      planting: 'There are three main types. Plant June-bearing for one big crop in late spring or early summer, everbearing for spring and fall crops, and day-neutral for a large crop in spring and smaller harvests all summer.',
+      image: ''
+    },
+    {
+      id: 22,
+      name: 'Peaches',
+      type: 'fruit',
+      planting: 'Plant trees against a southfacing wall under an eave, and prune them into a fan shape. And if you’re short on space, plant three or four varieties in one hole, pruning off all but the outwardfacing branches.',
+      image: ''
+    },
+    {
+      id: 23,
+      name: 'Pansies',
+      type: 'flower',
+      planting: 'Pansies like full or partial sun, but need cooler temperatures to thrive. The ideal planting site will get morning sun but avoid the heat of the late afternoon.',
+      image: ''
+    },
+    {
+      id: 24,
+      name: 'Hellebores',
+      type: 'flower',
+      planting: 'Hellebores prefer to grow in rich well-drained soil in full to part shade and whilst they are shade lovers most do better with a little sun, under the canopy of a deciduous tree or shrub is an ideal location.',
+      image: ''
+    },
     ]
   }
   
