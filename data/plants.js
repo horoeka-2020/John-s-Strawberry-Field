7b04a101204a8d40a53ac08bc87aef912e11c61d
Caz <cjpitcher@gmail.com>
2020-11-05 21:57:44 +1300

    Added plants js file

---
+++ data/plants.js
@@ -0,0 +1,157 @@
+const plants = {
+    Spring: [
+      {
+        id: 1,
+        name: '',
+        type: 'vegetable',
+        planting: ''
+      },
+      {
+        id: 2,
+        name: '',
+        type: 'vegetable',
+        planting: ''
+      },
+      {
+        id: 3,
+        name: '',
+        type: 'fruit',
+        planting: ''
+      },
+      {
+        id: 4,
+        name: '',
+        type: 'fruit',
+        planting: ""
+      },
+      {
+        id: 5,
+        name: '',
+        type: 'flower',
+        planting: ''
+      },
+      {
+        id: 6,
+        name: '',
+        type: 'flower',
+        planting: ''
+      },
+    ],
+    Summer: [
+    {
+        id: 7,
+        name: '',
+        type: 'vegetable',
+        planting: ''
+        },
+        {
+        id: 8,
+        name: '',
+        type: 'vegetable',
+        planting: ''
+        },
+        {
+        id: 9,
+        name: '',
+        type: 'fruit',
+        planting: ''
+        },
+        {
+        id: 10,
+        name: '',
+        type: 'fruit',
+        planting: ""
+        },
+        {
+        id: 11,
+        name: '',
+        type: 'flower',
+        planting: ''
+        },
+        {
+        id: 12,
+        name: '',
+        type: 'flower',
+        planting: ''
+        },
+    ],
+    Autumn: [
+    {
+        id: 13,
+        name: '',
+        type: 'vegetable',
+        planting: ''
+        },
+        {
+        id: 14,
+        name: '',
+        type: 'vegetable',
+        planting: ''
+        },
+        {
+        id: 15,
+        name: '',
+        type: 'fruit',
+        planting: ''
+        },
+        {
+        id: 16,
+        name: '',
+        type: 'fruit',
+        planting: ""
+        },
+        {
+        id: 17,
+        name: '',
+        type: 'flower',
+        planting: ''
+        },
+        {
+        id: 18,
+        name: '',
+        type: 'flower',
+        planting: ''
+        },
+    ],
+    Winter: [
+    {
+        id: 19,
+        name: '',
+        type: 'vegetable',
+        planting: ''
+        },
+        {
+        id: 20,
+        name: '',
+        type: 'vegetable',
+        planting: ''
+        },
+        {
+        id: 21,
+        name: '',
+        type: 'fruit',
+        planting: ''
+        },
+        {
+        id: 22,
+        name: '',
+        type: 'fruit',
+        planting: ""
+        },
+        {
+        id: 23,
+        name: '',
+        type: 'flower',
+        planting: ''
+        },
+        {
+        id: 24,
+        name: '',
+        type: 'flower',
+        planting: ''
+        },
+    ]
+  }
+  
+  export default plants
+  
