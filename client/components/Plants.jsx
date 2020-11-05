e4337c7999e04c9880a29d614b1ee239ce49e55e
Kittisan P <70613193+Kittisan-P@users.noreply.github.com>
2020-11-06 10:27:02 +1300

    Add Home nad Plants files and code to our project

---
+++ client/components/Plants.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+// import plants from '../../data/plants'
+// import { Link } from 'react-router-dom'
+
+// const Plants = () => {
+//   const rankKeys = Object.keys(ranks)
+//   return (
+//     <div>
+//       <h2>Nav</h2>
+//       <ul>
+//         {rankKeys.map((rank) => {
+//           return <Link to={`/list/${rank}`}>
+//             <li key={rank}>{rank}</li>
+//           </Link>
+//         })}
+
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default Plants
